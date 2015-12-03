20bd662df0f3ca6416322dd3fb8e8734c31c77d4
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-12-03 02:31:48 +0000

    Client API

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -8,22 +8,49 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Validator;
 
 class ClientController extends Controller
 {
     public function index()
     {
-        // return response()->json(Client::all());
-        return view('client.index')->with('clients', Client::all());
+        return response()->json(Client::all());
+    }
+
+    public function find(Client $client)
+    {
+        return response()->json($client->get());
+    }
+
+    public function projectsFrom(Client $client)
+    {
+        return response()->json($client->projects()->get());
     }
 
     public function save(Request $request)
     {
+        $validation = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
 
-    }
+        if ($validation->fails()) {
+            return response()->json($validation->getMessageBag()->all());
+        }
 
-    public function find($id)
-    {
-        return response()->json(Client::findOrFail($id));
+        try {
+            $client = Client::updateOrCreate([
+                'id' => $request->get('id')
+            ], $request->all());
+
+            return response()->json($client);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => Lang::get('errors.not_saved', [
+                    'model' => 'client'
+                ]),
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
