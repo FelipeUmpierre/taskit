e0a7c816b1b803875cd2de68ac428050ed959723
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-12-03 02:32:07 +0000

    Project API

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Client;
+use App\Project;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Project::all());
+    }
+
+    public function find(Project $project)
+    {
+        return response()->json($project->get());
+    }
+
+    public function clientFrom(Project $project)
+    {
+        return response()->json($project->client()->get());
+    }
+
+    public function tasksFrom(Project $project)
+    {
+        return response()->json($project->tasks()->get());
+    }
+
+    public function save()
+    {
+
+    }
+}
