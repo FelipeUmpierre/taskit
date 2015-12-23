e1135a13a794700b76fb7f37dbec60fe4f5a16af
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-12-23 00:27:13 +0000

    Added @dump function for Blade template

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * Blade directive to dump a variable/object inside a template.
+         * This is similar to dd(), except that it doesn't interrupt the
+         * execution of the app. It does NOT support multiple arguments
+         * however, you have to use one directive per variable.
+         *
+         * @example @dump($posts->comments)
+         */
+        Blade::directive('dump', function($param) {
+            return '<?php (new \Illuminate\Support\Debug\Dumper)->dump{$param}; ?>';
+        });
     }
 
     /**
